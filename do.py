234183c064bc259137e15058de35047d5d0ad84b
Fabio Arthur Ebsi von Schelling Goldman <f.vonschellinggoldman@campus.tu-berlin.de>
2018-03-01 17:54:20 +0100

    Python3

--- do.py
+++ do.py
@@ -16,33 +16,33 @@ def main():
     arg = str(sys.argv[1])
 
     if arg is None:
-        print "no argument given, exiting"
-        sys.exit
+        print("no argument given, exiting")
+        sys.exit()
     elif arg == "e":
-        print "editing"
+        print("editing")
         edit()
     elif arg == "c":
-        print "counting"
-        print "TOTAL WORDS TRANSLATED BY PETER:"
-        print count_words(os.getcwd() , 0, [])
-        print "-------------------------------"
+        print("counting")
+        print("TOTAL WORDS TRANSLATED BY PETER:")
+        print(count_words(os.getcwd(), 0, []))
+        print("-------------------------------")
     elif arg == "d":
-        print "duplicating"
+        print("duplicating")
         dir_walker(os.getcwd() )
     elif arg == "s":
-        print "searching"
+        print("searching")
         search_string(os.getcwd() )
     elif arg == "m":
-        print "moving"
+        print("moving")
         move()
     elif arg == "r":
-        print "replacing"
+        print("replacing")
         replace()
     else:
         print_helper()
 
-    print "great success"
-    
+    print("great success")
+
 
 def dir_walker(path):   #searches given path for file, walks backwards when unsuccessful
 
@@ -59,14 +59,14 @@ def copy_rename(name) :
         return
 
     bashcmd = "cp " + name + " " + adjusted_filename(name)
-    print bashcmd
+    print(bashcmd)
     os.system(bashcmd)
     
 
     return 0
 
 def adjusted_filename(name):
-    suffix = raw_input("Add suffix to copied file? : ")
+    suffix = input("Add suffix to copied file? : ")
     return "'" +name[0: name.index(".")] + suffix + name[name.index("."):] +"'" 
 
 def wrong_filetype(name) :
@@ -78,18 +78,17 @@ def wrong_filetype(name) :
     
 
 def edit():
-    yn = raw_input("Standard googletranslate file edit ? (y/n) : ")
+    yn = input("Standard googletranslate file edit ? (y/n) : ")
     if (yn.lower() == "y"):
         find_replace()
         return
 
-    yn = raw_input(".text to .md reformat ? (y/n) : ")
+    yn = input(".text to .md reformat ? (y/n) : ")
     if (yn.lower() == "y"):
         txt_to_md()
-        return    
+        return
 
-    print "said no to all options, do nothing."
-
+    print("said no to all options, do nothing.")
 
 
 def txt_to_md():
@@ -100,12 +99,12 @@ def txt_to_md():
     for root, dirs, files in os.walk(path):
         #if root != path:
         #    break
-        print files
+        print(files)
         for file in files:
             if file.endswith(".text") or  file.endswith(".txt"):
                 with open(os.path.join(root, file), 'r') as tarfile :
                     filedata = tarfile.read()
-                    print file
+                    print(file)
 
                     # Replace the target string
                 for i in range(len(searchText)):
@@ -114,9 +113,9 @@ def txt_to_md():
                         if (searchText[i] not in filedata):
                             continue
                         replace = filedata[filedata.index(searchText[i]):]
-                        print replace
+                        print(replace)
                         replace = replace[: replace.index("\n\n")]
-                        print replace
+                        print(replace)
                         if (searchText[i] == "bc."):
                             filedata = filedata.replace(replace, replace + "\n" + replaceText[i] )
                             filedata = filedata.replace(searchText[i], replaceText[i])
@@ -126,12 +125,11 @@ def txt_to_md():
                     else:
                         filedata = filedata.replace(searchText[i], replaceText[i])
 
-
-                    print searchText[i] + replaceText[i]
+                    print(searchText[i] + replaceText[i])
 
                 newfile = os.path.join(root, file[0:file.index(".")]+".md")
                 bashcmd = "cp " + "'"+ os.path.join(root, file) +"'"+ " " + "'"+newfile+"'"
-                print bashcmd
+                print(bashcmd)
                 os.system(bashcmd)
 
                     # Write the file out again
@@ -149,18 +147,18 @@ def ggt_to_md():
     for root, dirs, files in os.walk(path):
         if root != path:
             break
-        print files
+        print(files)
         for file in files:
             if "(EN_US)" in file:
 
                 with open(file, 'r') as tarfile :
                     filedata = tarfile.read()
-                    print file
+                    print(file)
 
                     # Replace the target string
                 for i in range(len(searchText)):
                     filedata = filedata.replace(searchText[i], replaceText[i])
-                    print searchText[i] + replaceText[i]
+                    print(searchText[i] + replaceText[i])
 
                     # Write the file out again
                 with open(file, 'w') as tarfile:
@@ -168,12 +166,12 @@ def ggt_to_md():
 
 
 def count_words(path, words, visited):
-    mustContain = raw_input("What must filename conatin? : ")
+    mustContain = input("What must filename conatin? : ")
     for root, dirs, files in os.walk(path):
         
         for file in files:
             if mustContain in file:
-                print file
+                print(file)
                 #visited.append(file)
                 with open(os.path.join(root,file)) as fh:
                     words += len(fh.read().split())
@@ -184,8 +182,8 @@ def count_words(path, words, visited):
 
 def search_string(path):
 
-    args = raw_input("what search for plz? : ").split(",")
-    print args
+    args = input("what search for plz? : ").split(",")
+    print(args)
     for root, dirs, files in os.walk(path):
         for name in files:
             searchable = args
@@ -197,9 +195,9 @@ def search_string(path):
                 str = e
                 #print "I/O error({0}): {1} could not open {2} ".format(e.errno, e.strerror, str(root)+str(name))
             except ValueError:
-                print "Could not convert data to an integer."
+                print("Could not convert data to an integer.")
             except:
-                print "Unexpected error:", sys.exc_info()[0]
+                print("Unexpected error:", sys.exc_info()[0])
                 raise
 
 
@@ -224,32 +222,32 @@ def results_printer(root, name, result):
     print(os.path.join(root, name)) + result
 
 def move():
-    filename = raw_input("filename plz, with extension : ")
-    dest = raw_input("destination plz, just folder name : ")
+    filename = input("filename plz, with extension : ")
+    dest = input("destination plz, just folder name : ")
     filePath = find(filename, os.getcwd())
-    print "moving file : " + filePath
+    print("moving file : " + filePath)
     possibleDestinations = []
     for root, dirs, files in os.walk(os.getcwd()):
         for dirnames in dirs:
             if dirnames == dest:
                 possibleDestinations.append(os.path.join(root, dirnames))
     x =1
-    print "-------------------------"
-    print "Possible Destinations : "
+    print("-------------------------")
+    print("Possible Destinations : ")
     for dest in possibleDestinations:
-        print str(x) + ". " + dest
+        print(str(x) + ". " + dest)
         x+=1
 
     selecting = True
     while(selecting) :
 
-        selection = int(raw_input("please select one : "))
-        if selection > len(possibleDestinations) or selection < 1 : 
-            print "Did not enter a correct number, please try again"
+        selection = int(input("please select one : "))
+        if selection > len(possibleDestinations) or selection < 1 :
+            print("Did not enter a correct number, please try again")
         else:
             selecting = False
 
-    print "to destination : " + possibleDestinations[selection-1]
+    print("to destination : " + possibleDestinations[selection - 1])
     bashcmd = "mv " + filePath + " " + possibleDestinations[selection-1]
     os.system(bashcmd)
 
@@ -270,18 +268,18 @@ def find(name, path):   #searches given path for file, walks backwards when unsu
 
 def replace():
     while (True):
-        searchText = raw_input("Text to Replace : ")
-        replaceText = raw_input("Replace with : ")
+        searchText = input("Text to Replace : ")
+        replaceText = input("Replace with : ")
 
         path = os.getcwd() 
         for root, dirs, files in os.walk(path):
             if root != path:
                 break
-            print files
+            print(files)
             for file in files:
                 with open(file, 'r') as tarfile :
                     filedata = tarfile.read()
-                    print file
+                    print(file)
 
                     # Replace the target string
                 for i in range(len(searchText)):
@@ -291,7 +289,7 @@ def replace():
                 with open(file, 'w') as tarfile:
                     tarfile.write(filedata)
 
-        again = raw_input("again? (y/n) : ")
+        again = input("again? (y/n) : ")
         if (again.lower() != "y"):
             break
 
